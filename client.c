e67a254ffc9ed3be669f8caeb4c503d391d22220
Brooke Wenig <brookewenig@gmail.com>
2017-02-26 17:45:04 -0800

    Init

---
+++ client.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>      // define structures like hostent
+#include <stdlib.h>
+#include <strings.h>
+
+#define MAX_PACKET_LEN 102
+#define MAX_SEQ_NUM 30720
+#define WINDOW_SIZE 5120
+#define RTO 500
+#define HEADER_SIZE 20
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd; //Socket descriptor
+    int portno, n;
+    struct sockaddr_in cli_addr, serv_addr;
+    struct hostent *server; //contains tons of information, including the server's IP address
+
+    char buffer[MAX_PACKET_LEN];
+    if (argc < 4) {
+       fprintf(stderr,"usage %s hostname port filename\n", argv[0]);
+       exit(0);
+    }
+    
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); //create a new socket
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    memset((char *) &cli_addr, 0, sizeof(cli_addr));
+    cli_addr.sin_family = AF_INET; //initialize server's address
+    cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    cli_addr.sin_port = htons(0);
+    
+    if (bind(sockfd,(struct sockaddr *)&cli_addr,sizeof(cli_addr)) < 0) //establish a connection to the server
+        error("ERROR bind failed hihihihi");
+
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET; //initialize server's address
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+    int serv_len = sizeof(serv_addr);
+
+    memset(buffer,0, MAX_PACKET_LEN);
+    char* filename = argv[3];
+    //printf("File to send: %s", filename);
+
+
+
+
+    printf("Sending packet SYN\n");
+    n = sendto(sockfd,"SYN",4, 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (n < 0) {error("ERROR sending to socket");}
+
+    n = recvfrom(sockfd,buffer,MAX_PACKET_LEN-1, 0, (struct sockaddr *)&serv_addr, &serv_len);
+    if (n < 0) {error("ERROR reading from socket");}
+    printf("Received SYN From server");
+
+    n = sendto(sockfd,filename,strlen(filename), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (n < 0) error("ERROR writing to socket");
+    
+	FILE * data;
+	
+	//char fin[6];
+	//strcpy(fin, "FIN");
+        memset(buffer,0,MAX_PACKET_LEN);
+	int seq_num = 0; //UPDATE!!!!
+	
+        n = recvfrom(sockfd,buffer,MAX_PACKET_LEN-1, 0, (struct sockaddr *)&serv_addr, &serv_len); //read from the socket
+	//printf("n: %d", n);
+        if (n < 0) {error("ERROR reading from socket");}
+        //printf("%s\n",buffer);	//print server's response
+    /*
+    	if(strcmp(buffer, fin)) {
+    		n = sendto(sockfd,"FINACK", 7, 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));		
+    		break;	
+    	}
+    */
+	printf("Receiving packet %d\n", seq_num);
+	data = fopen("received.data", "a");
+	//printf("buffer client: %s", buffer);
+	//Need to parse out sequence number
+    	fprintf(data, buffer);
+	
+	fclose(data);
+	
+    
+    close(sockfd); //close socket
+    
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/wait.h>	/* for the waitpid() system call */
+#include <signal.h>	/* signal name macros, and the kill() prototype */
+
+#define MAX_PACKET_LEN 102
+#define MAX_SEQ_NUM 30720
+#define WINDOW_SIZE 5120
+#define RTO 500
+#define HEADER_SIZE 20
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+  int sockfd, newsockfd, portno, pid;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
+  clilen = sizeof(cli_addr);
+
+  if (argc < 2) {
+     fprintf(stderr,"ERROR, no port provided\n");
+     exit(1);
+  }
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);	//create socket
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  memset((char *) &serv_addr, 0, sizeof(serv_addr));	//reset memory
+  //fill in address info
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+          error("ERROR on binding");
+	
+  int seq_num = 0;
+  while(1){
+    int n;
+    char buffer[MAX_PACKET_LEN];
+     
+    memset(buffer, 0, MAX_PACKET_LEN);	//reset memory
+
+    //read client's message
+    n = recvfrom(sockfd,buffer, MAX_PACKET_LEN, 0, (struct sockaddr *)&cli_addr, &clilen);
+    if(buffer == "SYN") {
+	printf("Sending packet %d %d SYN\n", seq_num, WINDOW_SIZE);
+    }
+    if (n < 0) error("ERROR reading from socket");
+    //printf("Here is the message: %s\n",buffer);
+    //buffer contains filename now
+    FILE *fp;
+    fp = fopen(buffer, "rb");
+    if(fp != NULL) {
+      fseek(fp, 0, SEEK_END);
+      long file_length = ftell(fp);
+      rewind(fp);
+	memset(buffer, 0, MAX_PACKET_LEN);
+	char buff1[HEADER_SIZE];
+	snprintf(buff1, HEADER_SIZE, "%d", seq_num);	
+	fread(buffer, sizeof(char), MAX_PACKET_LEN-HEADER_SIZE-1, fp);
+	//printf("buffer: %s", buffer);
+	printf("Sending packet %d %d\n", seq_num, WINDOW_SIZE);
+	n = sendto(sockfd,strcat(buff1, buffer),MAX_PACKET_LEN-1, 0, (struct sockaddr *)&cli_addr, clilen);
+	seq_num++;
+	if (n < 0) {
+	  error("ERROR writing to socket");
+	}	
+    }
+    else {
+  	printf("Nope. Try another file.");
+    }
+    fclose(fp);
+    printf("Sending packet %d %d FIN\n", seq_num, WINDOW_SIZE);
+    break;
+   // n = sendto(sockfd,"FIN", 4, 0, (struct sockaddr *)&cli_addr, clilen);
+   //n = sendto(sockfd,"I got your message",18, 0, (struct sockaddr *)&cli_addr, clilen);
+   //if (n < 0) error("ERROR writing to socket");
+  }
+     
+ close(sockfd); 
+ return 0; 
+}
+
